f6d02d39f0d326b336068b94c10ea8c3c94f146e
Thomas Lebeau <lebeau.thomas@gmail.com>
2015-09-13 19:43:30 +0300

    Add message

--- messages.js
+++ messages.js
@@ -5,6 +5,7 @@ module.exports = {
 		'Oh Snap!',
 		'Ooops I did it again!',
 		'Hasta la vista Baby',
-		'Talk to the hand'
+		'Talk to the hand',
+		'I\'m sorry Dave, I\'m affraid there is nothing there'
 	]
 };
