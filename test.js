49c364cae1ddc436c3ded8fba023551bb5dc531d
Thomas Lebeau <lebeau.thomas@gmail.com>
2015-09-13 19:48:06 +0300

    tweaks

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 'use strict';
 var assert = require('assert');
-var <%= camelModuleName %> = require('./');
+var zut = require('./');
 
 it('should ', function () {
-	assert.strictEqual(<%= camelModuleName %>('unicorns'), 'unicorns & rainbows');
+	assert.strictEqual(zut('unicorns'), 'unicorns & rainbows');
 });
