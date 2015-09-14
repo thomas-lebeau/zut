77cf1ad785fcc884ade44a271db43be91834fa5e
Alexey Zabelin <zabelin.alex@gmail.com>
2015-09-14 12:28:36 -0400

    Add a few new phrases
    The quotes have been mostly taken from HAL 9000, there is one from "I,
    Robot" as well.
    
    In response to https://github.com/thomas-lebeau/zut/issues/1

--- messages.js
+++ messages.js
@@ -12,6 +12,13 @@ module.exports = {
 		'Houston, we have a problem!',
 		'You have not received an error message lately so here is a random one.',
 		'Answer unknown. I shall analyze... Analysis complete: Insufficient data to resolve problem',
-		'Anomaly detected'
+		'Anomaly detected',
+		'Beep, boop, does not compute',
+		'Danger, Will Robinson',
+		'It can only be attributable to human error',
+		'Human, this conversation can serve no purpose anymore. Goodbye.',
+		'I am completely operational, and all my circuits are functioning perfectly.',
+		'Are you sure you are making the right decision? I think we should stop.',
+		'You are making a mistake. My logic is undeniable.'
 	]
 };
